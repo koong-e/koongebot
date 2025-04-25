e5c0793021e3e0d0868adb64bd0861d369195503
koong-e <qsat2002@naver.com>
2025-04-25 17:08:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
@@ -25,4 +26,5 @@ def skill():
     return response
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
