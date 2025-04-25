1502cecef5a57ee328bb358ddad313a2da25b68e
koong-e <qsat2002@naver.com>
2025-04-25 16:15:33 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "쿵이봇 작동 중!"
+
+@app.route('/skill', methods=['POST'])
+def skill():
+    return jsonify({
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "멍멍! 쿵이예요!"
+                    }
+                }
+            ]
+        }
+    })
+
+if __name__ == '__main__':
+    app.run()
