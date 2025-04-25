6ec295643c5101b8a34e84cd7a6f46a7da6336ce
koong-e <qsat2002@naver.com>
2025-04-25 16:57:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "쿵이봇 작동 중!"
+    return '쿵이봇 작동 중!'
 
 @app.route('/skill', methods=['POST'])
 def skill():
+    body = request.get_json()  # 요청 JSON 받아오기
+    utter = body['userRequest']['utterance']  # 사용자가 말한 내용 꺼내기
+
+    # 간단한 로직 예시 (여기서 응답 커스터마이즈 가능)
+    if '쿵이' in utter:
+        text = "멍멍! 쿵이예요!"
+    else:
+        text = "무슨 말인지 잘 모르겠어요 🐶"
+
     res = {
         "version": "2.0",
         "template": {
             "outputs": [
                 {
                     "simpleText": {
-                        "text": "멍멍! 쿵이예요!"
+                        "text": text
                     }
                 }
             ]
